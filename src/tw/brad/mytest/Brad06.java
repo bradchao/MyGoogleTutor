8156c78cdbd28a06903da42303a5bd0e5bd60767
brad <mybradtw@gmail.com>
2019-03-28 16:20:53 +0800

    2019032804

--- src/tw/brad/mytest/Brad06.java
+++ src/tw/brad/mytest/Brad06.java
@@ -1,24 +1,33 @@
 package tw.brad.mytest;
 
+import javax.swing.*;
+
 public class Brad06 {
     public static void main(String[] args){
-        int year = 2019;
-        if (year % 4 == 0){
-            if (year % 100 == 0){
-                if (year % 400 == 0){
-                    // 2000
+        String strYear = JOptionPane.showInputDialog("Input ur name");
+
+        try {
+            int year = Integer.parseInt(strYear);
+            System.out.println(year);
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        // 2000
+                        // ok
+                    } else {
+                        // 2100
+                        // xx
+                    }
+                } else {
+                    // 2016
                     // ok
-                }else{
-                    // 2100
-                    // xx
                 }
-            }else{
-                // 2016
-                // ok
+            } else {
+                // 2019
+                // xx
             }
-        }else{
-            // 2019
-            // xx
+        }catch (Exception e){
+            System.out.println("別鬧了");
         }
     }
 }
